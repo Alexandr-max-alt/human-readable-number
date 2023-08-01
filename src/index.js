91437a06db4e8c3a28b5a7acc845c313013235d2
Alexandr-max-alt <doberman8636@yandex.ru>
2023-08-01 14:47:42 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    
+    if (number === 0) {
+    return 'zero';
+    }
+    
+    function convertToWords(num) {
+        if (num < 10) {
+        return units[num];
+        } else if (num < 20) {
+        return teens[num - 10];
+        } else if (num < 100) {
+        return tens[Math.floor(num / 10)] + ' ' + units[num % 10];
+        } else if (num < 1000) {
+        return units[Math.floor(num / 100)] + ' hundred ' + convertToWords(num % 100);
+        } 
+    }
+    
+    let res = convertToWords(number);
+    return res.trim();
 }
